0c32e04469430bcb3a47cbb2a0e821b85989c709
Charles Silva <charlesk12019@gmail.com>
2025-11-03 08:38:39 -0300

    Nike

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
 
-function mudarvisual(cor, imagem){
-    body.style.background = cor
-    tenis.src = imagem
+function mudarvisual(cor, imagem, modelo){
+    const tenis = document.querySelector(".imagem-tenis");
+
+    body.style.backgroundColor = cor;
+    tenis.src = imagem;
+
+    tenis.classList.remove("nike1", "nike2", "nike3", "nike4");
+
+    tenis.classList.add(modelo);
 }
 
